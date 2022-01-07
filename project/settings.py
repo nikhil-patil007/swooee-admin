fc3ebd1ed53b3e7f0368dffc61ff0769e2080950
Nikhil Patil <nikhilpatil3701@gmail.com>
2022-01-07 15:53:46 +0530

    Final Commit.

--- project/settings.py
+++ project/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         # 'HOST': 'auth-db663.hostinger.com',
-#         'NAME': 'swooee-admin',
-#         'USER' : 'postgres',
-#         'PASSWORD' : 'Maquinistas',
-#         'PORT' : 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'HOST': 'auth-db663.hostinger.com',
+        'NAME': 'swooee-admin',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Maquinistas',
+        'PORT' : 5432,
+    }
+}
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
