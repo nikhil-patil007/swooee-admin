1906dd5d28a290f71a625a92c24a5dbf58f10cb6
Nikhil Patil <nikhilpatil3701@gmail.com>
2022-01-07 18:12:17 +0530

    Final

--- project/settings.py
+++ project/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 from django.contrib.messages import constants as messages
 
