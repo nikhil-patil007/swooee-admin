dccdd2a6e8f007c8a5ce277744c94548f1e47a1e
Nikhil Patil <nikhilpatil3701@gmail.com>
2022-01-11 10:26:41 +0530

    Final Commit

--- project/settings.py
+++ project/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 from django.contrib.messages import constants as messages
 
